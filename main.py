f0c0624dbf39900834d95f88cf40420352df0b5b
a.barkook <alibarkook@gmail.com>
2023-01-24 18:31:17 +0330

    update itreation and version

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-import gymnasium as gym
-env = gym.make("ALE/Pong-v5")
-observation, info = env.reset(seed=42)
-# gym.make()
+import random
+import gym
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+UP_ACTION = 2
+DOWN_ACTION = 3
+
+
+env = gym.make("Pong-v0")
+observation = env.reset()
+
+
+# for i in range(22):
+#     action = random.randint(2, 3) # this is where you would insert your policy
+  
+#     if i > 20:
+#         plt.imshow(observation)
+#         plt.show()
+
+
+#     observation, reward, terminated, info = env.step(action)
+#     print(f"reward: {reward} for action {action}")
+
 
 
 for _ in range(1000):
-    # print('actions:')
-    # print(env.action_space.sample())
-    action = env.action_space.sample() # this is where you would insert your policy
-    # print(env.step( action))
-    observation, reward, terminated, truncated, info = env.step( action)
+    action = random.randint(UP_ACTION, DOWN_ACTION) # this is where you would insert your policy
+
+
+    observation, reward, terminated, info = env.step(action)
     print(f"reward: {reward} for action {action}")
-    if terminated or truncated:
-        observation, info = env.reset()
+    # if terminated or truncated:
+    #     observation, info = env.reset()
 
 
 env.close()
